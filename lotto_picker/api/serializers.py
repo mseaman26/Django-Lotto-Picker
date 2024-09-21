1e3e2a848e6fabb4ebfd7aeda76c07c887f8a78e
michael seaman <mseaman26@gmail.com>
2024-09-20 23:46:59 -0700

    server setup with user and lottopick models

---
+++ lotto_picker/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import LottoPick
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password')
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
+
+class LottoPickSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LottoPick
+        fields = '__all__'
